31b86510eaf285da8bff7cfeb6f73d310eb0e912
Gülendamcetin <gulendamcetin970@gmail.com>
2025-03-18 22:13:27 +0300

    dtos added

--- SignalR.DtoLayer/obj/Debug/net8.0/SignalR.DtoLayer.AssemblyInfo.cs
+++ SignalR.DtoLayer/obj/Debug/net8.0/SignalR.DtoLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR.DtoLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3167521509f509bc61d42b6914eb5a23c581e04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62eb43e46d13cb557053dbcffb5c0ee1bd31bb9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR.DtoLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR.DtoLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
