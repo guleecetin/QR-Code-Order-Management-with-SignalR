eb0ce810e7c5ce2c09dca4cf99e539e77cc07fcd
Gülendamcetin <gulendamcetin970@gmail.com>
2025-03-18 11:33:16 +0300

    Entities were created.

---
+++ source/repos/SignalRProject/SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.Entitylayer.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+
+
+    }
+}
